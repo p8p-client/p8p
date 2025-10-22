31f30e6cc812420d82538849e7f0f80721dabc67
Julien Jacottet <jjacottet@gmail.com>
2025-10-22 21:49:35 +0200

    setting up splitsh for subrepos

---
+++ subtree.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -xe
+
+FORCE_FLAG=""
+
+while getopts "f" opt; do
+  case $opt in
+    f)
+      FORCE_FLAG="-f"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+
+# The first argument is now the path to composer.json
+COMPOSER_JSON_PATH=$1
+# The second argument is the git ref
+GIT_REF=$2
+
+if [ -z "$COMPOSER_JSON_PATH" ] || [ -z "$GIT_REF" ]; then
+    echo "Usage: $0 [-f] <path-to-composer.json> <git-ref>"
+    exit 1
+fi
+
+package=$(jq -r .name "$COMPOSER_JSON_PATH")
+directory=$(dirname "$COMPOSER_JSON_PATH")
+
+# Transform p8p/<package> to p8p-php/<package> for GitHub repository name
+github_package=$(echo "$package" | sed 's|p8p/|p8p-client/|')
+repository="https://github.com/$github_package"
+
+# Add the remote only if it doesn't exist already
+if ! git remote get-url "$package" > /dev/null 2>&1; then
+    git remote add "$package" "$repository"
+fi
+
+sha=$(splitsh-lite --prefix="$directory")
+git push $FORCE_FLAG "$package" "$sha":"$GIT_REF"
+
+echo "\n\n\n=======================================\n\n\n"
+
+
